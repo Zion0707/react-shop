59c1be36b14574b44c83bd91b6dd45aae0e6ac5f
huangzihao <346445684@qq.com>
2020-12-23 10:07:29 +0800

    添加redux-devtool工具

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducers from './reducers/index';
 
-const store = createStore(reducers, {});
+// 激活 Redux Devtools 工具，前提是需要在浏览器中添加 Redux Devtools 工具。
+const composeEnhanceser = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhanceser());
 
 export default store;
